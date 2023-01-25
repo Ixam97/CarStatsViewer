73d5464ddef04f1b2f0df415de93d6dece8a2463
boessi <dario.bosshard@flicks.ch>
2023-01-26 00:14:36 +0100

    Fix Scaling

--- automotive/src/main/java/com/ixam97/carStatsViewer/plot/PlotLine.kt
+++ automotive/src/main/java/com/ixam97/carStatsViewer/plot/PlotLine.kt
@@ -154,10 +154,10 @@ class PlotLine(
             else -> {
                 var maxByData = dataPoints.maxOf { it.bySecondaryDimension(secondaryDimension) }
 
-                if (!applyRange) return maxByData
-
-                if (baseConfiguration.Range.minPositive != null) maxByData = maxByData.coerceAtLeast(baseConfiguration.Range.minPositive)
-                if (baseConfiguration.Range.maxPositive != null) maxByData.coerceAtMost(baseConfiguration.Range.maxPositive)
+                if (applyRange) {
+                    if (baseConfiguration.Range.minPositive != null) maxByData = maxByData.coerceAtLeast(baseConfiguration.Range.minPositive)
+                    if (baseConfiguration.Range.maxPositive != null) maxByData = maxByData.coerceAtMost(baseConfiguration.Range.maxPositive)
+                }
 
                 maxByData
             }
@@ -182,10 +182,10 @@ class PlotLine(
             else -> {
                 var minByData = dataPoints.minOf { it.bySecondaryDimension(secondaryDimension) }
 
-                if (!applyRange) return minByData
-
-                if (baseConfiguration.Range.minNegative != null) minByData = minByData.coerceAtMost(baseConfiguration.Range.minNegative)
-                if (baseConfiguration.Range.maxNegative != null) minByData.coerceAtLeast(baseConfiguration.Range.maxNegative)
+                if (applyRange) {
+                    if (baseConfiguration.Range.minNegative != null) minByData = minByData.coerceAtMost(baseConfiguration.Range.minNegative)
+                    if (baseConfiguration.Range.maxNegative != null) minByData = minByData.coerceAtLeast(baseConfiguration.Range.maxNegative)
+                }
 
                 minByData
             }
