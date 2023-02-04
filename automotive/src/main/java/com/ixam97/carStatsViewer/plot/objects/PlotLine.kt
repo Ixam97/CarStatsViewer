6aa1da80fe891f1884f1192920431dd37f0be707
Dario Bosshard <dario.bosshard@fefundinfo.com>
2023-02-04 12:12:37 +0100

    BEGIN_SESSION on first item

--- automotive/src/main/java/com/ixam97/carStatsViewer/plot/objects/PlotLine.kt
+++ automotive/src/main/java/com/ixam97/carStatsViewer/plot/objects/PlotLine.kt
@@ -46,7 +46,7 @@ class PlotLine(
             prev?.Marker = PlotLineMarkerType.END_SESSION
         }
         
-        if (dataPoint.Marker == null && prev?.Marker == PlotLineMarkerType.END_SESSION) {
+        if (dataPoint.Marker == null && (prev?.Marker == null || prev?.Marker == PlotLineMarkerType.END_SESSION)) {
             dataPoint.Marker = PlotLineMarkerType.BEGIN_SESSION
         }
 
