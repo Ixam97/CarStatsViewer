8407000c63dd2e9136f4fb85466e9e25f82c19e4
Ixam97 <maxigoldschmidt@gmail.com>
2023-03-12 16:23:36 +0100

    Add SMTP login to API settings

--- automotive/src/main/java/com/ixam97/carStatsViewer/activities/SettingsApisActivity.kt
+++ automotive/src/main/java/com/ixam97/carStatsViewer/activities/SettingsApisActivity.kt
@@ -1,7 +1,10 @@
 package com.ixam97.carStatsViewer.activities
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
 import com.ixam97.carStatsViewer.CarStatsViewer.Companion.liveDataApis
 import com.ixam97.carStatsViewer.R
 import com.ixam97.carStatsViewer.appPreferences.AppPreferences
@@ -25,9 +28,33 @@ class SettingsApisActivity: Activity() {
         settings_apis_abrp_token.setOnClickListener {
             liveDataApis[0].showSettingsDialog(this@SettingsApisActivity)
         }
+
+        settings_apis_smtp_login.setOnClickListener {
+            showSmtpLoginDialog()
+        }
     }
 
     private fun showSmtpLoginDialog() {
+        val credentialsDialog = AlertDialog.Builder(this@SettingsApisActivity).apply {
+            val layout = LayoutInflater.from(this@SettingsApisActivity).inflate(R.layout.dialog_smtp_credentials, null)
+            val smtp_dialog_address = layout.findViewById<EditText>(R.id.smtp_dialog_address)
+            smtp_dialog_address.setText(appPreferences.smtpAddress)
+            val smtp_dialog_password = layout.findViewById<EditText>(R.id.smtp_dialog_password)
+            smtp_dialog_password.setText(appPreferences.smtpPassword)
+            val smtp_dialog_server = layout.findViewById<EditText>(R.id.smtp_dialog_server)
+            smtp_dialog_server.setText(appPreferences.smtpServer)
 
+            setView(layout)
+
+            setPositiveButton("OK") { dialog, _ ->
+                appPreferences.smtpAddress = smtp_dialog_address.text.toString()
+                appPreferences.smtpPassword = smtp_dialog_password.text.toString()
+                appPreferences.smtpServer = smtp_dialog_server.text.toString()
+            }
+            setTitle("SMTP Login")
+            setCancelable(true)
+            create()
+        }
+        credentialsDialog.show()
     }
 }
