0480f91d12494142744cc2d2a2bc4e878df02472
Maximilian Goldschmidt <ixam97@ixam97.de>
2023-07-26 20:18:19 +0200

    Added new MultiButtonWidget

---
+++ automotive/src/main/java/com/ixam97/carStatsViewer/ui/views/MultiButtonWidget.kt
@@ -0,0 +1,93 @@
+package com.ixam97.carStatsViewer.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
+import com.ixam97.carStatsViewer.R
+
+class MultiButtonWidget @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): LinearLayout(context, attrs, defStyleAttr) {
+
+    fun interface OnIndexChangedListener {
+        fun indexChanged()
+    }
+
+    private var onIndexChangedListener: OnIndexChangedListener? = null
+
+    val buttonList: List<Button>
+    val dividerList: List<View>
+
+    var maxIndex: Int = 3
+        set(value) {
+            field = value
+            setupVisibleButtons()
+        }
+
+    var selectedIndex: Int = 0
+        set(value) {
+            field = value
+            updateView()
+        }
+
+    init {
+        inflate(context, R.layout.widget_multi_button, this)
+
+        buttonList = listOf(
+            findViewById(R.id.button_1),
+            findViewById(R.id.button_2),
+            findViewById(R.id.button_3),
+            findViewById(R.id.button_4)
+        )
+
+        dividerList = listOf(
+            findViewById(R.id.divider_1),
+            findViewById(R.id.divider_2),
+            findViewById(R.id.divider_3),
+        )
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.MultiButtonWidget)
+
+        try {
+            maxIndex = attributes.getInteger(R.styleable.MultiButtonWidget_numButtons, 4) - 1
+            selectedIndex = attributes.getInteger(R.styleable.MultiButtonWidget_defaultIndex, 0)
+            buttonList[0].text = attributes.getString(R.styleable.MultiButtonWidget_textButton1)?: "Button 1"
+            buttonList[1].text = attributes.getString(R.styleable.MultiButtonWidget_textButton2)?: "Button 2"
+            buttonList[2].text = attributes.getString(R.styleable.MultiButtonWidget_textButton3)?: "Button 3"
+            buttonList[3].text = attributes.getString(R.styleable.MultiButtonWidget_textButton4)?: "Button 4"
+        } finally {
+            attributes.recycle()
+        }
+    }
+
+    fun setOnIndexChangedListener(listener: OnIndexChangedListener) {
+        onIndexChangedListener = listener
+    }
+
+    private fun setupVisibleButtons() {
+        buttonList.forEachIndexed { index, button ->
+            if (index > maxIndex) {
+                button.isVisible = false
+            } else {
+                button.setOnClickListener {
+                    selectedIndex = index
+                    onIndexChangedListener?.indexChanged()
+                }
+            }
+        }
+    }
+
+    private fun updateView() {
+        buttonList.forEachIndexed { index, button ->
+            button.isSelected = (index == selectedIndex)
+        }
+        dividerList.forEachIndexed { index, divider ->
+            divider.isVisible = (index != selectedIndex && index != selectedIndex - 1 && index < maxIndex)
+        }
+    }
+}
